0ca20104ab8d09308f2467bb697c2a2ca05c2cee
Masahiro Koizumi <masahiro.koizumi@qlonolink.com>
2020-12-07 14:18:47 +0900

    Add sample code

--- hoge/hoge.go
+++ hoge/hoge.go
@@ -11,3 +11,12 @@ func message() string {
 func Do() {
 	fmt.Println(message())
 }
+
+// ReferToInternalTest refers to internal test helper function
+// go build fails
+// [main @main]# go build
+// # github.com/mkql/hybrid_testing_go_sample/hoge
+// hoge/hoge.go:17:2: undefined: someHelperFunc
+func ReferToInternalTest() {
+	someHelperFunc()
+}
--- hoge/hoge_test.go
+++ hoge/hoge_test.go
@@ -8,3 +8,5 @@ func TestMessage(t *testing.T) {
 		t.Fatalf("want %s, but got %s", want, got)
 	}
 }
+
+func someHelperFunc(){}
